411d7d5fbea6ea3f9e0186c1bef859ba4321385f
Satoru_Murata <17216462+Natch@users.noreply.github.com>
2018-02-11 14:30:14 +0900

    🆙 commit-test

---
+++ study.localhost/hello.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: satoru
+ * Date: 2018/02/06
+ * Time: 3:35
+ */
+?>
+
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Hello, World</title>
+</head>
+<body>
+    <p><?php echo "Hello, I'm a PHP!"; ?></p>
+</body>
+</html>
