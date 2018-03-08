74cbb69bd0076e17cee40efd7be7ac64658d95e5
Satoru_Murata <17216462+Natch@users.noreply.github.com>
2018-03-09 01:29:42 +0900

    👕 perfectPHP - P178不要なコードスタイルを削除

--- study.localhost/online_bbs/bbs.php
+++ study.localhost/online_bbs/bbs.php
@@ -40,7 +40,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 
     if (count($errors) === 0) {
-        $sql = "INSERT INTO `post` (`name`, `comment`, `created_at`) VALUES ('"
+        $sql = "INSERT INTO post (name, comment, created_at) VALUES ('"
             . mysqli_real_escape_string($link, $name) . "', '"
             . mysqli_real_escape_string($link, $comment) . "', '"
             . date('Y-m-d H:i:s') . "')";
@@ -66,7 +66,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <ul class="error_list">
                 <?php foreach ($errors as $error): ?>
                 <li>
-                    <?php echo htmlspecialchars($error, ENT_QUOTES, 'UTF-8') ?>
+                    <?php echo htmlspecialchars($error, ENT_QUOTES); ?>
                 </li>
                 <?php endforeach; ?>
             </ul>
@@ -76,16 +76,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <input type="submit" name="submit" value="送信" />
         </form>
         <?php
-        $sql = "SELECT * FROM `post` ORDER BY `created_at` DESC";
+        $sql = 'SELECT * FROM post ORDER BY created_at DESC';
         $result = mysqli_query($link, $sql);
         ?>
         <?php if ($result !== false && mysqli_num_rows($result)): ?>
         <ul>
             <?php while ($post = mysqli_fetch_assoc($result)): ?>
             <li>
-                <?php echo htmlspecialchars($post['name'], ENT_QUOTES, 'UTF-8'); ?>:
-                <?php echo htmlspecialchars($post['comment'], ENT_QUOTES, 'UTF-8'); ?> -
-                <?php echo htmlspecialchars($post['created_at'], ENT_QUOTES, 'UTF-8'); ?>
+                <?php echo htmlspecialchars($post['name'], ENT_QUOTES); ?>:
+                <?php echo htmlspecialchars($post['comment'], ENT_QUOTES); ?> -
+                <?php echo htmlspecialchars($post['created_at'], ENT_QUOTES); ?>
             </li>
             <?php endwhile; ?>
         </ul>
